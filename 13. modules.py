922d3533ff011196e04f000781ba2fc3725c12d5
taichikuji <ivanperezfernandezsuper@gmail.com>
2020-06-04 13:54:53 +0200

    explaining modules in a nutshell

---
+++ 13. modules.py
@@ -0,0 +1,3 @@
+import function_for_modules
+
+function_for_modules.ahyes("Kuji")
---
+++ function_for_modules.py
@@ -0,0 +1,7 @@
+print("module file: 13. modules.py")
+print("source file: function_for_modules.py")
+
+
+# module for 13. modules.py
+def ahyes(name):
+    print("Gamer: " + name)
